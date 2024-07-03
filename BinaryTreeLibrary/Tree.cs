4b26fd345689f949769d0b90fce6061430d4eb19
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-07-02 22:56:01 -0300

    Initial commit

---
+++ BinaryTreeLibrary/Tree.cs
@@ -0,0 +1,72 @@
+﻿using BinaryTreeLibrary;
+
+public class Tree
+{
+    private TreeNode root;
+
+    // Constructor
+    public Tree()
+    {
+        root = null;
+    }
+
+    // InsertNode method
+    public void InsertNode(int insertValue)
+    {
+        if (root == null)
+            root = new TreeNode(insertValue);
+        else
+            root.Insert(insertValue);
+    }
+
+    // PreorderTraversal method
+    public void PreorderTraversal()
+    {
+        PreorderHelper(root);
+    }
+
+    // Helper method for preorder traversal
+    private void PreorderHelper(TreeNode node)
+    {
+        if (node != null)
+        {
+            Console.Write(node.Data + " ");
+            PreorderHelper(node.LeftNode);
+            PreorderHelper(node.RightNode);
+        }
+    }
+
+    // InorderTraversal method
+    public void InorderTraversal()
+    {
+        InorderHelper(root);
+    }
+
+    // Helper method for inorder traversal
+    private void InorderHelper(TreeNode node)
+    {
+        if (node != null)
+        {
+            InorderHelper(node.LeftNode);
+            Console.Write(node.Data + " ");
+            InorderHelper(node.RightNode);
+        }
+    }
+
+    // PostorderTraversal method
+    public void PostorderTraversal()
+    {
+        PostorderHelper(root);
+    }
+
+    // Helper method for postorder traversal
+    private void PostorderHelper(TreeNode node)
+    {
+        if (node != null)
+        {
+            PostorderHelper(node.LeftNode);
+            PostorderHelper(node.RightNode);
+            Console.Write(node.Data + " ");
+        }
+    }
+}
---
+++ BinaryTreeLibrary/TreeTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class TreeTest
+{
+    public static void Main(string[] args)
+    {
+        Tree tree = new Tree();
+        int insertValue;
+        Random random = new Random();
+
+        Console.WriteLine("Inserting values:");
+        for (int i = 1; i <= 10; i++)
+        {
+            insertValue = random.Next(100);
+            Console.Write(insertValue + " ");
+            tree.InsertNode(insertValue);
+        }
+
+        Console.WriteLine("\n\nPreorder traversal:");
+        tree.PreorderTraversal();
+
+        Console.WriteLine("\n\nInorder traversal:");
+        tree.InorderTraversal();
+
+        Console.WriteLine("\n\nPostorder traversal:");
+        tree.PostorderTraversal();
+
+        Console.WriteLine();
+    }
+}
